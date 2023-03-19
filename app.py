dbb23b07f1d7ff80beef675e66405233641d0da3
Chelsea Griffo <chelseagriffo@gmail.com>
2023-03-19 18:23:34 -0500

    Added routes to app.py

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 from flask import (render_template, redirect, 
                    url_for, request)
 from models import db, Project, app
+
+
+@app.route('/')
+def index():
+    return f'Thank you for viewing my programming portfolio!'
+
+
+@app.route('/project/new')
+def create():
+    pass
+
+
+@app.route('/project/<id>')
+def detail():
+    pass
+
+
+@app.route('/project/<id>/edit')
+def edit_project():
+    pass
+
+
+@app.route('/project/<id>/delete')
+def delete_project():
+    pass
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000, host='0.0.0.0')
