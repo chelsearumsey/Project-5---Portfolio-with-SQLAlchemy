89b65c931bd82a14c58c1993424716ff1fc19ac5
Chelsea Griffo <chelseagriffo@gmail.com>
2023-03-19 17:16:50 -0500

    Created db model and Project class

--- models.py
+++ models.py
@@ -5,3 +5,14 @@ import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
 db = SQLAlchemy(app)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column('Title', db.String())
+    date = db.Column('Date', db.Date())
+    description = db.Column('Project Description', db.Text)
+    skills = db.Column('Skills', db.Text)
+
+    def __repr__(self):
+        return f'Title: {self.title} Date: {self.date} Project Description: {self.description} Skills: {self.skills}'
