dfcabdbb4dd0d1a2c3abefe6ab0ef8a745941aeb
Chelsea Griffo <chelseagriffo@gmail.com>
2023-03-25 03:25:51 -0500

    Tried adding for loop to add projects to db

--- app.py
+++ app.py
@@ -4,21 +4,7 @@ from models import db, Project, app
 
 
 def insert_project_data():
-        project1= Project({db.id:1, db.title:'Number Guessing Game', 
-                    db.date:'November 2022', 
-                    db.description: f'''For this project, I created a random number 
-                                    guessing game in which the player is asked to: 
-                                    "Pick a number between 1 and 10". The player's 
-                                    job is to make a guess, and the program will tell 
-                                    them whether their guess is too high or too low. 
-                                    Their next guess is based on what the program has 
-                                    told them. The game ends when the player guesses 
-                                    the correct number. Usually, players will try to 
-                                    do this in the lowest number of tries possible.''',
-                    db.skills: f'''Catching exceptions, Creating a loop, 
-                                    Accepting user input, Conditional statements''',
-                    db.url: 'https://github.com/chelsearumsey/Project-1---Number-Guessing-Game'})
-        project2= Project({db.id:1, db.title:'Basketball Stats Tool', 
+    projects = [{id:2, title:'Basketball Stats Tool', 
                     db.date:'January 2023', 
                     db.description: f'''For this project, I was given a list of teams and players, 
                                         tasked with cleaning up the player data, and then organizing 
@@ -32,8 +18,8 @@ def insert_project_data():
                     db.skills: f'''Cleaning data, Appending data to multiple lists, 
                                     Catching exceptions, Creating a loop, Accepting 
                                     user input, Conditional statements''',
-                    db.url: 'https://github.com/chelsearumsey/Project-2---Basketball-Stats-Tool'})
-        project3= Project({db.id:1, db.title:'Phrase Hunter', 
+                    db.url: 'https://github.com/chelsearumsey/Project-2---Basketball-Stats-Tool'},
+                    {id:3, db.title:'Phrase Hunter', 
                     db.date:'February 2023', 
                     db.description: f'''For this project, I created a word guessing game: 
                                         "Phrase Hunter." I used Python and object-oriented 
@@ -42,8 +28,8 @@ def insert_project_data():
                                         phrase by inputting individual characters.''',
                     db.skills: f'''Object oriented programming, Creating loops, Accepting 
                                     user input, Conditional statements ''',
-                    db.url: 'https://github.com/chelsearumsey/Project-3---Phrase-Hunter'})
-        project4= Project({db.id:1, db.title:'Store Inventory with SQLAlchemy', 
+                    db.url: 'https://github.com/chelsearumsey/Project-3---Phrase-Hunter'},
+                    {id:4, db.title:'Store Inventory with SQLAlchemy', 
                     db.date:'March 2023', 
                     db.description: f'''For this project, I built a console application that 
                                         allows you to easily interact with data for a store's 
@@ -53,9 +39,40 @@ def insert_project_data():
                                         records, and exporting a new CSV backup.''',
                     db.skills: f'''Used knowledge of CSV, File I/O, SQL, and Git, Set up a virtual environment 
                                     with Python, Cleaned CSV data, and Created a database using SQLAlchemy''',
-                    db.url: 'https://github.com/chelsearumsey/Project-4---Store-Inventory-with-SQLAlchemy'})
-        db.session.add(project1, project2, project3, project4)
-        db.session.commit()
+                    db.url: 'https://github.com/chelsearumsey/Project-4---Store-Inventory-with-SQLAlchemy'}] 
+    for project in projects:
+        project_in_db = db.session.query(Project).filter(Project.title==project[title]).one_or_none()
+        if project_in_db == None:
+            id = project[id]
+            title = project[title]
+            date = project[date]
+            description = project[description]
+            skills = project[skills]
+            url= project[url]
+            new_project = Project(id=id, title=title, date=date, 
+                                    description=description, 
+                                    skills=skills, url=url)
+            db.session.add(new_project)
+            db.session.commit()
+        # project1= Project(
+        #             id=1, 
+        #             title='Number Guessing Game', 
+        #             date='November 2022', 
+        #             description=f'''For this project, I created a random number 
+        #                             guessing game in which the player is asked to: 
+        #                             "Pick a number between 1 and 10". The player's 
+        #                             job is to make a guess, and the program will tell 
+        #                             them whether their guess is too high or too low. 
+        #                             Their next guess is based on what the program has 
+        #                             told them. The game ends when the player guesses 
+        #                             the correct number. Usually, players will try to 
+        #                             do this in the lowest number of tries possible.''',
+        #             skills=f'''Catching exceptions, Creating a loop, 
+        #                             Accepting user input, Conditional statements''',
+        #             url='https://github.com/chelsearumsey/Project-1---Number-Guessing-Game')
+        
+        # db.session.add(project1)
+        # db.session.commit()
 
 @app.route('/')
 def index():
@@ -94,4 +111,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=8000, host='0.0.0.0')
+    with app.app_context():
+        insert_project_data()
     
